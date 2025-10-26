3a7fce3581ecfc6c5ccb5b56732fb182cb5a9572
Yuki <pietrayuki5@gmail>
2025-10-26 17:08:47 -0300

    add txt/html; mod html; del py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('cadastro_morador.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
