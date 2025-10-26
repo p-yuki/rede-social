3397ab60bdb8e89afb6e76aaef7fb25b8872f10e
Yuki <pietrayuki5@gmail>
2025-10-26 20:49:34 -0300

    mod all

--- app.py
+++ app.py
@@ -22,4 +22,3 @@ if __name__ == '__main__':
     from flask import Flask
 
 app = Flask(__name__)
-
--- static/js/base.js
+++ static/js/base.js
@@ -2,4 +2,5 @@ const hamburger = document.querySelector("#toggle-btn");
 
 hamburger.addEventListener("click", function() {
     document.querySelector("#sidebar").classList.toggle("expand");
+    hamburger.classList.toggle("rotated");
 });
