0dd5038cb85a31492504d9b98251e258cd65b498
p-yuki <pietrayuki5@gmail.com>
2025-10-27 08:48:47 -0300

    mod html

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('painel.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
