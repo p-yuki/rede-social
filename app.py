d6670f3f383faf6a2a036ca98b91f7c5a589c5c6
p-yuki <pietrayuki5@gmail.com>
2025-11-03 16:21:26 -0300

    mod all

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from models import db
+from utils import login_required
 import os
 
 app = Flask(__name__)
@@ -25,30 +26,59 @@ app.register_blueprint(aeps_bp)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Senai%40118@localhost/redesocialdb'
 db.init_app(app)
 
+@app.context_processor
+def inject_usuario():
+    class Usuario:
+        def __init__(self, nome, bloco, apartamento, is_adm):
+            self.nome = nome
+            self.bloco = bloco
+            self.apartamento = apartamento
+            self.is_adm = is_adm
+
+    usuario = Usuario(
+        session.get('user_name'),
+        session.get('user_bloco'),
+        session.get('user_apartamento'),
+        session.get('is_adm')
+    )
+
+    return dict(usuario=usuario)
+
+
 @app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/painel')
+@login_required
 def painel():
     return render_template('painel.html')
 
 @app.route('/achados_perdidos')
+@login_required
 def achados_perdidos():
     return render_template('achados_perdidos.html')
 
 @app.route('/trabalhos')
+@login_required
 def trabalhos():
     return render_template('trabalhos.html')
 
 @app.route('/perfil')
+@login_required
 def perfil():
     return render_template('perfil.html')
 
+
+@app.route('/usuarios')
+@login_required
+def novo_user():
+    return render_template('usuarios.html')
+
 @app.route('/chat')
+@login_required
 def chat():
     return render_template('chat.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    from flask import Flask
-
-app = Flask(__name__)
-
