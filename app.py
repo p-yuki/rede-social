4486333e8f9a133cd2cb227fefcb2e07757f301e
geovanna romig <geovannaromig@hotmail.com>
2025-12-02 08:22:23 -0300

    app corrigido

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.secret_key = 'sua_chave_secreta_123'
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 app.config['SECRET_KEY'] = '123'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode="threading")
 app.config['UPLOAD_FOLDER'] = os.path.join(BASE_DIR, 'static', 'uploads')
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
